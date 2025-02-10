7a984477017cfe67f0d22112e7b63a917d8094cc
Anderton <23054103@myrp.edu.sg>
2025-02-10 15:52:31 +0800

    update, refresh

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,24 +5,39 @@ import { useRecipesContext } from "../hooks/useRecipesContext"
 import RecipeDetails from "../components/RecipeDetails"
 import RecipeForm from "../components/RecipeForm"
 import SearchBar from '../components/SearchBar';
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Home = () => {
   const { recipes, dispatch } = useRecipesContext()
+  const { user } = useAuthContext();
 
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchRecipes = async () => {
-      const response = await fetch('/api/recipes')
-      const json = await response.json()
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/recipes`, {
+          headers: {
+            Authorization: `Bearer ${user.token}`, // Include user token for authorization
+          },
+        });
 
-      if (response.ok) {
-        dispatch({ type: 'SET_RECIPES', payload: json })
+        const json = await response.json();
+
+        if (response.ok) {
+          dispatch({ type: "SET_RECIPE", payload: json });
+        } else {
+          console.error("Failed to fetch recipes:", json.error);
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching recipes:", error);
       }
-    }
+    };
 
-    fetchRecipes()
-  }, [dispatch])
+    if (user) {
+      fetchRecipes();
+    }
+  }, [dispatch, user]);
 
   const filteredItems = recipes
     ? recipes.filter((item) => item.name.toLowerCase().includes(searchQuery.toLowerCase()))
@@ -31,12 +46,12 @@ const Home = () => {
   return (
     <div className="home">
       <div>
-      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <div className="workouts">
-        {filteredItems.map(recipe => (
-          <RecipeDetails recipe={recipe} key={recipe._id} />
-        ))}
-      </div>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <div className="workouts">
+          {filteredItems.map(recipe => (
+            <RecipeDetails recipe={recipe} key={recipe._id} />
+          ))}
+        </div>
       </div>
       <RecipeForm />
     </div>
