88daa200cfd5383df7e612ef4e81b9c79d1b5307
s777ia <23046128@myrp.edu.sg>
2025-02-10 11:51:43 +0800

    update navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+          <h1>Recipe Management</h1>
         </Link>
       </div>
     </header>
